3b6362861ee583ba881ecfffb91bb65c707ba0be
waqax94 <waqax94@gmail.com>
2020-08-13 04:06:49 +1000

    api push

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Service;
+use App\User;
+use App\WatcherRelation;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ApiController extends Controller
+{
+    public function testConnection() {
+        return response()->json("Connection Okay", 200);
+    }
+
+    public function wearerLoginProcessing(Request $request) {
+        $wearerPhone = $request->wearerPhone;
+        $wearerPassword = $request->wearerPassword;
+
+
+        if(DB::table('users')->where('phone', '=', $wearerPhone)->exists()){
+
+            $user = DB::table('users')->where('phone', '=', $wearerPhone)->first();
+
+            if(Hash::check($wearerPassword, $user->password)){
+
+                //user name and password matched
+
+                if(DB::table('services')->where('wearer_id', '=', $user->person_id)->where('wom_num', '=', $wearerPhone)->exists()){
+
+                    // service exists
+
+                    if(DB::table('services')->where('wearer_id', '=', $user->person_id)->where('service_status', '=', 'Active')->exists()){
+
+                        $service = DB::table('services')->where('wearer_id', '=', $user->person_id)->where('service_status', '=', 'Active')->first();
+
+                        // login sucessfull
+
+                        $data = array(
+                            'wearerId' => $user->person_id,
+                            'serviceId' => $service->service_id,
+                            'wearerFirstName' => $user->f_name,
+                            'wearerLastName' => $user->l_name,
+                            'wearerEmail' => $user->email,
+                            'wearerPhone' => $user->phone
+                        );
+
+                        $res = array (
+                            'connection' => true,
+                            'queryStatus' => true,
+                            'message' => "Login Successful",
+                            'data' => $data
+                        );
+
+                        return response()->json($res,200);
+
+
+                    } else {
+                        // service is not active
+
+                        $data = array(
+                            'wearerId' => "",
+                            'serviceId' => "",
+                            'wearerFirstName' => "",
+                            'wearerLastName' => "",
+                            'wearerEmail' => "",
+                            'wearerPhone' => ""
+                        );
+
+                        $res = array (
+                            'connection' => true,
+                            'queryStatus' => false,
+                            'message' => "Service is not yet active",
+                            'data' => $data
+                        );
+                        return response()->json($res,200);
+                    }
+
+
+                } else {
+
+                    // service does not exist
+                    $data = array(
+                        'wearerId' => "",
+                        'serviceId' => "",
+                        'wearerFirstName' => "",
+                        'wearerLastName' => "",
+                        'wearerEmail' => "",
+                        'wearerPhone' => ""
+                    );
+
+                    $res = array (
+                        'connection' => true,
+                        'queryStatus' => false,
+                        'message' => "User is not a wearer",
+                        'data' => $data
+                    );
+
+                    return response()->json($res,200);
+                }
+
+            } else {
+
+                $data = array(
+                    'wearerId' => "",
+                    'serviceId' => "",
+                    'wearerFirstName' => "",
+                    'wearerLastName' => "",
+                    'wearerEmail' => "",
+                    'wearerPhone' => ""
+                );
+                $res = array (
+                    'connection' => true,
+                    'queryStatus' => false,
+                    'message' => "Incorrect password",
+                    'data' => $data
+                );
+
+                return response()->json($res,200);
+            }
+
+        } else {
+
+            $data = array(
+                'wearerId' => "",
+                'serviceId' => "",
+                'wearerFirstName' => "",
+                'wearerLastName' => "",
+                'wearerEmail' => "",
+                'wearerPhone' => ""
+            );
+
+            $res = array (
+                'connection' => true,
+                'queryStatus' => false,
+                'message' => "User does not exits",
+                'data' => $data
+            );
+
+            return response()->json($res,200);
+
+        }
+
+        $data = array(
+            'wearerId' => "",
+            'serviceId' => "",
+            'wearerFirstName' => "",
+            'wearerLastName' => "",
+            'wearerEmail' => "",
+            'wearerPhone' => ""
+        );
+
+        $res = array (
+            'connection' => false,
+            'queryStatus' => false,
+            'message' => "Connection Error",
+            'data' => $data
+        );
+
+        return response()->json($res,500);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/testConnection','ApiController@testConnection')->name('api.test.connection');
+Route::post('/wearerLoginProcessing','ApiController@wearerLoginProcessing')->name('wearer.login.processing');

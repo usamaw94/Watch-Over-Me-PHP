584d8ca005d597137dab045655afe03987a563cc
waqax94 <waqax94@gmail.com>
2020-08-30 09:58:04 +1000

    new api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -478,7 +478,7 @@ class ApiController extends Controller
 
             try{
                 Mail::send('emails.contactWatcher', $data, function ($message) use ($data) {
-                    $message->from('mailtest2194@gmail.com', 'Watch Over Me');
+                    $message->from('watchoverme@uawdevstudios.com', 'Watch Over Me');
                     $message->to($data['watcherEmail']);
                     $message->subject('Watch Over Me - Help Me Request');
                 });
@@ -487,6 +487,7 @@ class ApiController extends Controller
             catch (Throwable $e){
 
             }
+
 
 
         } else {
